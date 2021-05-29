4cf2dd2bb125f0659a10971651e9aef3ce61afef
GordonFeng <hellogordon.f@gmail.com>
2021-05-29 13:52:54 +0800

    refactor test to remove duplication

--- EssentialFeedTests/RemoteFeedLoader.swift
+++ EssentialFeedTests/RemoteFeedLoader.swift
@@ -12,14 +12,6 @@ protocol HTTPClient {
     func get(form url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestURL: URL?
-
-    func get(form url: URL) {
-        requestURL = url
-    }
-}
-
 class RemoteFeedLoader {
 
     let client: HTTPClient
@@ -39,23 +31,33 @@ class RemoteFeedLoader {
 class RemoteFeedLoaderTest: XCTestCase {
 
     func testInitDoesNotRequestDataFromURL() {
-        let url = URL(string: "https://google.com")!
 
-        let clientSpy = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: clientSpy)
+        let (_, client) = makeSUT()
 
-        XCTAssertNil(clientSpy.requestURL)
+        XCTAssertNil(client.requestURL)
     }
 
     func testLoadRequestDataFromURL() {
         //check which url we are loading data from
         let url = URL(string: "https://yahoo.com.tw")!
 
-        let client = HTTPClientSpy()
-        let loader = RemoteFeedLoader(url: url, client: client)
+        let (loader, client) = makeSUT(url: url)
         loader.load()
 
         XCTAssertEqual(client.requestURL, url)
     }
 
+    //MARK: - Helper
+    private func makeSUT(url: URL = URL(string: "http://google.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        return (RemoteFeedLoader(url: url, client: client), client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestURL: URL?
+
+        func get(form url: URL) {
+            requestURL = url
+        }
+    }
 }
