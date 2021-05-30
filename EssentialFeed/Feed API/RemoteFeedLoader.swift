a769563f9e6268e88565f0f903b6cad00f664bb2
GordonFeng <hellogordon.f@gmail.com>
2021-05-30 15:09:38 +0800

    move mapping logic to new type `FeedItemsMapper`

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -34,11 +34,10 @@ public final class RemoteFeedLoader {
 
             switch result {
             case .success(let successItem):
-
-                if successItem.response.statusCode == 200,
-                   let root = try? JSONDecoder().decode(Root.self, from: successItem.data) {
-                    completion(.success(root.items.map({ $0.item })))
-                } else {
+                do {
+                    let feedItems = try FeedItemsMapper.map(successItem.data, successItem.response)
+                    completion(.success(feedItems))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure(_):
@@ -48,21 +47,31 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Codable {
-    let items: [Item]
-}
+private class FeedItemsMapper {
 
-//cause we will have feedItem from different source, remote source or local source, so we won't dependancy model on FeedItem itself but on RemoteFeedLoader and LocalFeedLoader seperate, so if server change api's property, will not effect whole FeedItem module
-private struct Item: Codable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    private struct Root: Codable {
+        let items: [Item]
+    }
 
-    var item: FeedItem {
-        return FeedItem(id: self.id,
-                        description: self.description,
-                        location: self.location,
-                        imageURL: self.image)
+    private struct Item: Codable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+
+        var item: FeedItem {
+            return FeedItem(id: self.id,
+                            description: self.description,
+                            location: self.location,
+                            imageURL: self.image)
+        }
+    }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map({ $0.item})
     }
 }
